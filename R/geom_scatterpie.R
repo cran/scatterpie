b59ee62641cc6e02d9ff7233a20b271b8e538b22
Guangchuang Yu <guangchuangyu@gmail.com>
2016-12-02 09:46:28 +0000

    version 0.0.5

---
+++ R/geom_scatterpie.R
@@ -0,0 +1,41 @@
+##' scatter pie plot
+##'
+##'
+##' @title geom_scatterpie
+##' @param mapping aes mapping
+##' @param data data
+##' @param cols cols the pie data
+##' @param ... additional parameters
+##' @importFrom ggforce geom_arc_bar
+##' @importFrom utils modifyList
+##' @importFrom tidyr gather_
+##' @importFrom ggplot2 aes_
+##' @export
+##' @return layer
+##' @examples
+##' library(ggplot2)
+##' d <- data.frame(x=rnorm(5), y=rnorm(5))
+##' d$A <- abs(rnorm(5, sd=1))
+##' d$B <- abs(rnorm(5, sd=2))
+##' d$C <- abs(rnorm(5, sd=3))
+##' ggplot() + geom_scatterpie(aes(x=x, y=y), data=d, cols=c("A", "B", "C")) + coord_fixed()
+##' @author guangchuang yu
+geom_scatterpie <- function(mapping=NULL, data, cols, ...) {
+    if (is.null(mapping))
+        mapping <- aes_(x=~x, y=~y)
+    mapping <- modifyList(mapping, aes_(r0=0, fill=~type,
+                                        amount=~value))
+
+    if (!'r' %in% names(mapping)) {
+        xvar <- as.character(mapping)["x"]
+        size <- diff(range(data[, xvar]))/50
+        mapping <- modifyList(mapping, aes_(r=size))
+    }
+
+    names(mapping)[match(c("x", "y"), names(mapping))] <- c("x0", "y0")
+
+    df <- gather_(data, "type", "value", cols)
+    geom_arc_bar(mapping, data=df, stat='pie', inherit.aes=FALSE, ...)
+}
+
+
---
+++ R/recenter.R
@@ -0,0 +1,27 @@
+##' re-center map data
+##'
+##'
+##' @title recenter
+##' @param mapdata map data, shoud be a data.frame
+##' @param center center
+##' @param longitude_column longitude column
+##' @return updated map data
+##' @export
+##' @author ygc
+recenter <- function(mapdata, center, longitude_column='long') {
+    if (center <= 0) {
+        stop("center should be positive value...")
+    }
+    md2 <- mapdata
+    md2$long <- md2$long + 360
+    md2$group <- md2$group + max(md2$group) + 1
+
+    mapdata <- rbind(mapdata, md2)
+    long <- mapdata[,longitude_column]
+
+    res <- subset(mapdata, long >= center-180 & long <= center+180)
+
+    return(res)
+}
+
+
